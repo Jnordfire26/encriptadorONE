5004e3cf9e1ab03b7e1ead917449097e1e7fea8d
Jnordfire26 <Juannicolas26062001@gmail.com>
2024-08-17 12:41:38 -0500

    Subida de encriptador

---
+++ assets/js/script.js
@@ -0,0 +1,134 @@
+/* 
+e - enter
+o - ober
+i - imes
+a - ai
+u - ufat
+*/
+
+const encriptar = document.getElementById("button__encriptar");
+const desencriptar = document.getElementById("button__desencriptar");
+const copy = document.getElementById("button__copiar");
+const textoInicial = document.getElementById("textoInput");
+const textFinal = document.getElementById("textoFinal");
+const munheco = document.getElementById("munheco");
+const textInfo = document.getElementById("textoInfo");
+const rigth = document.getElementById("rigth")
+const modal1 = document.getElementById("modal1");
+const modal2 = document.getElementById("modal2");
+const modal3 = document.getElementById("modal3");
+const closeModal1 = document.getElementsByClassName("close1")[0];
+const closeModal2 = document.getElementsByClassName("close2")[0];
+const closeModal3 = document.getElementsByClassName("close3")[0];
+	
+const remplace = (newvalue) => {
+	textFinal.innerHTML = newvalue;
+	textFinal.classList.add("ajustar");
+	rigth.classList.add("ajuste")
+	textoInicial.value = "";
+	textoInicial.style.height = "auto"
+	textoInicial.placeholder = "Ingrese el texto aquí";
+	munheco.classList.add("ocultar");
+	textInfo.classList.add("ocultar");
+	copy.classList.remove("bn_ocultar");
+}
+
+const reset = () => {
+	textoInicial.value = "";
+    textoInicial.style.height = "auto";
+	textFinal.innerHTML = "";
+	rigth.classList.remove("ajuste")
+	textFinal.classList.remove("ajustar");
+	munheco.classList.remove("ocultar");
+	textFinal.placeholder = "Ningún mensaje fue encontrado";
+	textInfo.classList.remove("ocultar")
+	copy.classList.add("bn_ocultar");
+	textoInicial.focus();
+};
+
+let remplazar = [
+	["e", "enter"],
+	["o", "ober"],
+	["i", "imes"],
+	["a", "ai"],
+	["u", "ufat"]
+];
+
+encriptar.addEventListener('click', () => {
+
+	const texto = textoInicial.value.toLowerCase();
+
+	if (texto != "") {
+		function encript(newtext) {
+			for (let i = 0; i < remplazar.length; i++) {
+				if (newtext.includes(remplazar[i][0])) {
+					newtext = newtext.replaceAll(remplazar[i][0], remplazar[i][1]);
+				};
+			};
+			return newtext;
+		};
+		remplace(encript(texto));
+	} else {
+		modal2.style.display = "block";
+		closeModal2.onclick = function() {
+			modal2.style.display = "none";
+			}
+		reset();
+	};
+});
+
+
+textoInicial.addEventListener("change", e => {
+	textoInicial.style.height = "auto";
+	let scHeight = e.target.scrollHeight;
+	textoInicial.style.height = `${scHeight}px`;
+});
+textoInicial.addEventListener("keyup", e => {
+	textoInicial.style.height = "auto";
+	let scHeight = e.target.scrollHeight;
+	textoInicial.style.height = `${scHeight}px`;
+});
+
+desencriptar.addEventListener('click', () => {
+
+	const texto = textoInicial.value.toLowerCase();
+
+	if (texto != "") {
+		function desencript(newtext) {
+			for (let i = 0; i < remplazar.length; i++) {
+				if (newtext.includes(remplazar[i][1])) {
+					newtext = newtext.replaceAll(remplazar[i][1], remplazar[i][0]);
+				};
+			};
+			return newtext;
+		};
+		remplace(desencript(texto));
+	} else {
+		
+		modal3.style.display = "block";
+
+		closeModal3.onclick = function() {
+			modal3.style.display = "none";
+			}
+
+		reset();
+	};
+});
+
+copy.onclick = function() {
+    modal1.style.display = "block";
+}
+
+
+closeModal1.onclick = function() {
+
+    modal1.style.display = "none";
+	
+}
+
+
+window.onclick = function(event) {
+    if (event.target == modal1) {
+        modal1.style.display = "none";
+    }
+}
